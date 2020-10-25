df3321e732153cf2a24e16b2b3729fa2b0de9da7
Pedro Capriles <zearkiatos@Pedros-MacBook-Air.local>
2020-10-25 09:24:23 -0300

    Searching for Videos

--- src/apis/youtube/index.js
+++ src/apis/youtube/index.js
@@ -1,1 +1,11 @@
-import { YOUTUBE_API_KEY } from '../../config/constants';
+import axios from 'axios';
+import { YOUTUBE_API_KEY, YOUTUBE_API_URL_BASE } from '../../config/constants';
+
+export default axios.create({
+    baseURL: YOUTUBE_API_URL_BASE,
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: YOUTUBE_API_KEY
+    }
+});
--- src/config/constants/index.js
+++ src/config/constants/index.js
@@ -1,5 +1,6 @@
 const config = {
-    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY
+    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
+    YOUTUBE_API_URL_BASE: process.env.YOUTUBE_API_URL_BASE
 }
 
 export default config;
