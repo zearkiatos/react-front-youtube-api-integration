3fe4910a3c113a510d36f2118f8462e09c3e0835
Pedro Capriles <zearkiatos@Pedros-MacBook-Air.local>
2020-12-05 16:25:33 -0300

    Displaying a Video Player

--- src/components/VideoDetail/index.js
+++ src/components/VideoDetail/index.js
@@ -4,8 +4,13 @@ const VideoDetail = ({ video }) => {
     if (!video) {
         return <div>Loading...</div>
     }
+
+    const videoSource = `https://www.youtube.com/embed/${video.id.videoId}`;
     return (
         <div>
+            <div className="ui embed">
+                <iframe src={videoSource} title={video.snippet.title} />
+            </div>
             <div className="ui segment">
                 <h4 className="ui header">
                     {video.snippet.title}
